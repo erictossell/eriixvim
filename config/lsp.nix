e41b024f5c573aad825fd537b759fe877f287e85
Eric Tossell <erictossell@gmail.com>
2024-01-01 01:15:03 -0500

    Linters

--- config/lsp.nix
+++ config/lsp.nix
@@ -4,12 +4,18 @@
     lsp = {
       enable = true;
       servers = {
+        ansiblels.enable = true;
+        astro.enable = true;
+	bashls.enable = true;
         cssls.enable = true;
+	dockerls.enable = true;
         gopls.enable = true;
         html.enable = true;
+	jsonls.enable = true;
         lua-ls.enable = true;
         nixd.enable = true;
         ruff-lsp.enable = true;
+	rust-analyzer.enable = true;
         terraformls.enable = true;
       };
     };
