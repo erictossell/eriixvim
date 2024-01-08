fd598ffae5264eab27fa80ca6f4572fa66c35f0c
erictossell <erictossell@users.noreply.github.com>
2024-01-08 18:57:15 +0000

    Auto lint/format

--- config/lsp.nix
+++ config/lsp.nix
@@ -9,20 +9,20 @@
       servers = {
         ansiblels.enable = true;
         astro.enable = true;
-	bashls.enable = true;
+        bashls.enable = true;
         cssls.enable = true;
-	dockerls.enable = true;
+        dockerls.enable = true;
         gopls.enable = true;
         html.enable = true;
-	jsonls.enable = true;
+        jsonls.enable = true;
         lua-ls.enable = true;
         nixd.enable = true;
         ruff-lsp.enable = true;
-	rust-analyzer = {
-	  enable = true;
-	  installCargo = true;
-	  installRustc = true;
-	};
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+          installRustc = true;
+        };
         terraformls.enable = true;
       };
     };
