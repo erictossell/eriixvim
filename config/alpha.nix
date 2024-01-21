72a8a5e964371f0a60f2052a4a92d6e1257fc7d0
Eric Tossell <erictossell@gmail.com>
2024-01-21 10:28:24 -0500

    Formatter

--- config/alpha.nix
+++ config/alpha.nix
@@ -25,9 +25,9 @@
           "    \\  \\:\\/:/     \\  \\:\\          /__/:/      /__/:/    \\  \\:\\      "
           "     \\  \\::/       \\  \\:\\         \\__\\/       \\__\\/      \\  \\:\\     "
           "      \\__\\/         \\__\\/                                 \\__\\/     "
-"			               "
+          "			               "
 
-	  "			eriim.dev | github.com/eriim                 "
+          "			eriim.dev | github.com/eriim                 "
 
 
         ];
@@ -37,103 +37,103 @@
         val = 2;
       }
       {
-            type = "button";
-            val = "󰺄  > New File";
-            on_press.raw = "require('telescope.builtin').find_files";
-            opts = {
-              # hl = "comment";
-              keymap = [ "n" "e" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
-              shortcut = "e";
+        type = "button";
+        val = "󰺄  > New File";
+        on_press.raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [ "n" "e" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
+          shortcut = "e";
 
-              position = "center";
-              cursor = 3;
-              width = 38;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
+          position = "center";
+          cursor = 3;
+          width = 38;
+          align_shortcut = "right";
+          hl_shortcut = "Keyword";
+        };
       }
 
 
-{
+      {
         type = "padding";
         val = 2;
       }
-{
-            type = "button";
-            val = "  > Find File";
-            on_press.raw = "require('telescope.builtin').find_files";
-            opts = {
-              # hl = "comment";
-              keymap = [ "n" "f" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
-              shortcut = "f";
+      {
+        type = "button";
+        val = "  > Find File";
+        on_press.raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [ "n" "f" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
+          shortcut = "f";
 
-              position = "center";
-              cursor = 3;
-              width = 38;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
+          position = "center";
+          cursor = 3;
+          width = 38;
+          align_shortcut = "right";
+          hl_shortcut = "Keyword";
+        };
       }
 
-{
+      {
         type = "padding";
         val = 2;
       }
-{
-            type = "button";
-            val = "  > Find Recent";
-            on_press.raw = "require('telescope.builtin').find_files";
-            opts = {
-              # hl = "comment";
-              keymap = [ "n" "t" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
-              shortcut = "t";
+      {
+        type = "button";
+        val = "  > Find Recent";
+        on_press.raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [ "n" "t" ":Telescope find_files <CR>" { noremap = true; silent = true; nowait = true; } ];
+          shortcut = "t";
 
-              position = "center";
-              cursor = 3;
-              width = 38;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
+          position = "center";
+          cursor = 3;
+          width = 38;
+          align_shortcut = "right";
+          hl_shortcut = "Keyword";
+        };
       }
-{
+      {
         type = "padding";
         val = 2;
       }
-{
-            type = "button";
-            val = " > Repos";
-            on_press.raw = "require('telescope.builtin').find_files";
-            opts = {
-              # hl = "comment";
-              keymap = [ "n" "r" ":cd $HOME/repos | Telescope find_files<CR>" { noremap = true; silent = true; nowait = true; } ];
-              shortcut = "r";
+      {
+        type = "button";
+        val = " > Repos";
+        on_press.raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [ "n" "r" ":cd $HOME/repos | Telescope find_files<CR>" { noremap = true; silent = true; nowait = true; } ];
+          shortcut = "r";
 
-              position = "center";
-              cursor = 3;
-              width = 38;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
+          position = "center";
+          cursor = 3;
+          width = 38;
+          align_shortcut = "right";
+          hl_shortcut = "Keyword";
+        };
       }
-{
+      {
         type = "padding";
         val = 2;
       }
-{
-            type = "button";
-            val = "󰗼  > Quit NVIM";
-            on_press.raw = "require('telescope.builtin').find_files";
-            opts = {
-              # hl = "comment";
-              keymap = [ "n" "q" ":qa<CR>" { noremap = true; silent = true; nowait = true; } ];
-              shortcut = "q";
+      {
+        type = "button";
+        val = "󰗼  > Quit NVIM";
+        on_press.raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [ "n" "q" ":qa<CR>" { noremap = true; silent = true; nowait = true; } ];
+          shortcut = "q";
 
-              position = "center";
-              cursor = 3;
-              width = 38;
-              align_shortcut = "right";
-              hl_shortcut = "Keyword";
-            };
+          position = "center";
+          cursor = 3;
+          width = 38;
+          align_shortcut = "right";
+          hl_shortcut = "Keyword";
+        };
       }
 
 
@@ -152,46 +152,46 @@
 
 
 
-     # {
-     #   type = "group";
-     #   val = [
-     #     {
-     #       command = "<CMD>ene <CR>";
-     #       desc = "  > New file";
-     #       shortcut = "e";
-     #     }
-     #     {
-     #       command = ":cd $HOME/repos | Telescope find_files<CR>";
-     #       desc = "󰺄  > Find file";
-     #       shortcut = "f";
-     #     }
-     #     {
-     #       command = ":Telescope oldfiles<CR>";
-     #       desc = "  > Find Recent";
-     #       shortcut = "t";
-     #     }
-     #     {
-     #       command = ":cd $HOME/repos | Telescope find_files<CR>";
-     #       desc = " > Repos";
-     #       shortcut = "r";
-     #     }
-     #     {
-     #       command = ":cd $HOME/docs | Neotree<CR>";
-     #       desc = "󱔗  > Docs";
-     #       shortcut = "d";
-     #     }
-     #     {
-     #       command = ":cd $HOME/repos/nix/nixflakes | Neotree <CR>";
-     #       desc = "  > Nixflakes";
-     #       shortcut = "n";
-     #     }
-     #     {
-     #       command = ":qa<CR>";
-     #       desc = "󰗼  > Quit NVIM";
-     #       shortcut = "q";
-     #     }
-     #   ];
-     # }
+      # {
+      #   type = "group";
+      #   val = [
+      #     {
+      #       command = "<CMD>ene <CR>";
+      #       desc = "  > New file";
+      #       shortcut = "e";
+      #     }
+      #     {
+      #       command = ":cd $HOME/repos | Telescope find_files<CR>";
+      #       desc = "󰺄  > Find file";
+      #       shortcut = "f";
+      #     }
+      #     {
+      #       command = ":Telescope oldfiles<CR>";
+      #       desc = "  > Find Recent";
+      #       shortcut = "t";
+      #     }
+      #     {
+      #       command = ":cd $HOME/repos | Telescope find_files<CR>";
+      #       desc = " > Repos";
+      #       shortcut = "r";
+      #     }
+      #     {
+      #       command = ":cd $HOME/docs | Neotree<CR>";
+      #       desc = "󱔗  > Docs";
+      #       shortcut = "d";
+      #     }
+      #     {
+      #       command = ":cd $HOME/repos/nix/nixflakes | Neotree <CR>";
+      #       desc = "  > Nixflakes";
+      #       shortcut = "n";
+      #     }
+      #     {
+      #       command = ":qa<CR>";
+      #       desc = "󰗼  > Quit NVIM";
+      #       shortcut = "q";
+      #     }
+      #   ];
+      # }
       {
         type = "padding";
         val = 2;
@@ -203,7 +203,7 @@
         };
         type = "text";
         val = "Inspiring quote here.";
-     }
+      }
     ];
   };
 }
--- flake.nix
+++ flake.nix
@@ -7,14 +7,15 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    nixvim,
-    flake-parts,
-    ...
-  } @ inputs: let
-    config = import ./config; # import the module directly
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    { nixvim
+    , flake-parts
+    , ...
+    } @ inputs:
+    let
+      config = import ./config; # import the module directly
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -22,34 +23,39 @@
         "aarch64-darwin"
       ];
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-        nvim = nixvim'.makeNixvimWithModule {
-          inherit pkgs;
-          module = config;
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-          extraSpecialArgs = {
-            # inherit (inputs) foo;
+      perSystem =
+        { pkgs
+        , system
+        , ...
+        }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nvim = nixvim'.makeNixvimWithModule {
+            inherit pkgs;
+            module = config;
+            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
           };
-        };
-      in {
-        checks = {
-          # Run `nix flake check .` to verify that your config is not broken
-          default = nixvimLib.check.mkTestDerivationFromNvim {
-            inherit nvim;
-            name = "A nixvim configuration";
+        in
+        {
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNvim {
+              inherit nvim;
+              name = "A nixvim configuration";
+            };
           };
-        };
 
-        packages = {
-          # Lets you run `nix run .` to start nixvim
-          default = nvim;
+          packages = {
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
+          };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ nixpkgs-fmt ];
+          };
         };
-      };
     };
 }
